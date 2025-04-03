51dcbe75cdc0d986479291d836b13ac777b41942
DevSeb <seb@kwiefreiheit.com>
2025-04-03 18:28:41 +0200

    allowed chunk sizing in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) { // Example: Split vendor libraries into their own chunk
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                }
+            }
+        }
+    },
     plugins: [react()],
     server: {
         allowedHosts: ['c992-2003-df-5704-3a00-dd8c-1311-91bf-2.ngrok-free.app']
